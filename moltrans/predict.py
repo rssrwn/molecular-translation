20f5159bfa30d4bdeea228ca06112c87f7fc7510
rssrwn <rssrwn@gmail.com>
2021-05-31 10:11:25 +0200

    Fix prediction script

--- moltrans/predict.py
+++ moltrans/predict.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import torch
 import pandas as pd
 from rdkit import Chem
 from pathlib import Path
@@ -7,11 +8,22 @@ from torch.utils.data import DataLoader
 
 import moltrans.util as util
 from moltrans.data import BMSTestDataset
-from moltrans.model import BMSEncoder, BMSDecoder, BMSModel
+from moltrans.tokeniser import Tokeniser
+from moltrans.sampler import DecodeSampler
+from moltrans.model import MoCoEncoder, BARTModel, BMSModel
 
+
+RANDOM_SEED = 42069
+VAL_SPLIT = 0.02
+CHEM_TOKEN_START = 26
+LOG_DIR = "tb_logs"
+REGEX = "\[[^\]]+]|Br?|Cl?|N|O|S|P|F|I|b|c|n|o|s|p|\(|\)|\.|=|#|-|\+|\\\\|\/|:|~|@|\?|>|\*|\$|\%[0-9]{2}|[0-9]"
 
 DEFAULT_VOCAB_PATH = "vocab.txt"
 DEFAULT_MAX_SEQ_LEN = 256
+DEFAULT_BATCH_SIZE = 128
+DEFAULT_ENC_PATH = "tb_logs/bms-encoder/version_1/checkpoints/last.ckpt"
+DEFAULT_DEC_PATH = "tb_logs/bms-decoder/version_0/checkpoints/last.ckpt"
 
 D_MODEL = 512
 D_FEEDFORWARD = 2048
@@ -30,8 +42,17 @@ def build_dataloader(args, dataset):
 
 
 def load_model(args, sampler):
-    encoder = BMSEncoder(D_MODEL)
-    decoder = BMSDecoder(D_MODEL, D_FEEDFORWARD, NUM_LAYERS, NUM_HEADS)
+    # Load encoder
+    encoder = MoCoEncoder.load_from_checkpoint(args.encoder_path)
+    encoder.encoder_k = None
+    encoder.enc_k_fc = None
+    encoder.queue = None
+    encoder.queue_ptr = None
+
+    # Load decoder
+    decoder = BARTModel.load_from_checkpoint(args.decoder_path)
+    decoder.encoder = None
+
     model = BMSModel.load_from_checkpoint(
         args.model_path,
         encoder=encoder,
@@ -53,7 +74,7 @@ def predict(model, loader):
     img_ids = []
     sampled_smiles = []
     for idx, (batch_img_ids, imgs) in enumerate(loader):
-        model_input = {"images": torch.stack(imgs)}
+        model_input = {"images": imgs.to(device)}
         mol_strs, _ = model.sample_molecules(model_input)
         img_ids.extend(batch_img_ids)
         sampled_smiles.extend(mol_strs)
@@ -77,7 +98,7 @@ def write_predictions(filename, img_ids, inchis):
 
 def main(args):
     print("Loading test dataset...")
-    test_dataset = BMSTestDataset.from_data_path(args.data_path, transform=util.TRANSFORM)
+    test_dataset = BMSTestDataset.from_data_path(args.data_path, transform=util.TEST_TRANSFORM)
     print("Loaded dataset.")
 
     print("Loading tokeniser and sampler...")
@@ -110,6 +131,9 @@ if __name__ == "__main__":
     parser.add_argument("--model_path", type=str)
     parser.add_argument("--vocab_path", type=str, default=DEFAULT_VOCAB_PATH)
     parser.add_argument("--max_seq_len", type=int, default=DEFAULT_MAX_SEQ_LEN)
+    parser.add_argument("--encoder_path", type=str, default=DEFAULT_ENC_PATH)
+    parser.add_argument("--decoder_path", type=str, default=DEFAULT_DEC_PATH)
+    parser.add_argument("--batch_size", type=int, default=DEFAULT_BATCH_SIZE)
 
     parsed_args = parser.parse_args()
     main(parsed_args)
